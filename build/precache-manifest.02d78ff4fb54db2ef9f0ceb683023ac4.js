edd1ac7b9347283dd20b31f780a997007504836a
Nuel822 <Clinton.bob72@yahoo.com>
2020-04-13 04:26:31 +0100

    deploy

---
+++ build/precache-manifest.02d78ff4fb54db2ef9f0ceb683023ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb645e00c1194a7e05249f6d7f31acd8",
+    "url": "/Covid19_VisualizationReact/index.html"
+  },
+  {
+    "revision": "0119e2346022dc2d40c4",
+    "url": "/Covid19_VisualizationReact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2b04256aa39bd1aa75d7",
+    "url": "/Covid19_VisualizationReact/static/css/main.ba993414.chunk.css"
+  },
+  {
+    "revision": "0119e2346022dc2d40c4",
+    "url": "/Covid19_VisualizationReact/static/js/2.ba4af8cc.chunk.js"
+  },
+  {
+    "revision": "b0351fe73ea0b4aa25b2c4d98a626b4a",
+    "url": "/Covid19_VisualizationReact/static/js/2.ba4af8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b04256aa39bd1aa75d7",
+    "url": "/Covid19_VisualizationReact/static/js/main.1ad11843.chunk.js"
+  },
+  {
+    "revision": "212965813b9261ed59c3",
+    "url": "/Covid19_VisualizationReact/static/js/runtime-main.432a945a.js"
+  }
+]);
