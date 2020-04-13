04adc397d132ea9e0499597256bb00962f2351aa
Nuel822 <Clinton.bob72@yahoo.com>
2020-04-13 05:01:40 +0100

    removed redux devtool

--- src/action/store.js
+++ src/action/store.js
@@ -2,7 +2,8 @@ import { compose,applyMiddleware,createStore } from "redux";
 import  thunk from "redux-thunk";
 import { reducers } from "../reducer";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer =  compose;
 
 export const store = createStore(
     reducers,
